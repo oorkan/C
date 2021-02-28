eda807dcf4014340edf2611e158e6063859db712
oorkan <oorkan.dev@gmail.com>
2021-03-01 03:58:07 +0400

    add 09-fahrenheit_to_celsius.4

---
+++ 09-fahrenheit_to_celsius.4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// The printf conversion specification %3.0f says that
+// a floating-point number (here fahr) is to be printed
+// at least three characters wide, with no decimal point and 
+// no fraction digits.
+// %6.1f describes another number (celsius) that is to be 
+// printed at least six characters wide, with one digit after 
+// the decimal point.
+
+void main()
+{
+  float fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 10;
+
+  fahr = lower;
+  printf("\n%4s\t%7s\n", "\u00B0C", "\u00B0F");
+  printf("----------------\n\n");
+  while (fahr <= upper) {
+    celsius = (5.0/9.0) * ( fahr - 32 );
+    printf("%3.0f\t%6.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+}
