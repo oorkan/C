6c5b4d1921e77e509a5fc7a5bc6aca73b7cb4088
oorkan <oorkan.dev@gmail.com>
2021-03-09 00:28:47 +0400

    add description to 20-character_count

--- 20-character_count.c
+++ 20-character_count.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/* The character counting program accumulates its count in a long
+variable instead of an int. long integers are at least 32 bits. Although
+on some machines, int and long are the same size, on others an int is 16
+bits, with a maximum value of 32767, and it would take relatively little
+input to overflow an int counter. The conversion specification %ld tells
+ printf that the corresponding argument is a long integer. */
+
 void main()
 {
   int c;
