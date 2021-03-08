2c7f9d73b61ab34cc288a7966a8360987fb89390
oorkan <oorkan.dev@gmail.com>
2021-03-09 00:34:40 +0400

    add 21-character_count.2

---
+++ 21-character_count.2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* It may also be possible to cope with even bigger numbers by using
+a double (double precision float). printf uses %f for both float and 
+double; %.0f suppresses printing of the decimal point and the fraction
+part which is zero in our case. */
+
+void main()
+{
+  int c;
+  double numchars;
+
+  numchars = 0;
+  while ( ( c = getchar() ) != EOF ) {
+    // omit new line chars
+    if (c != '\n') {
+      ++numchars;
+    }
+  }
+  printf("%.0f\n", numchars);
+}
