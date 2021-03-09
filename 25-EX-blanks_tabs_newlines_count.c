bce0ba1ba26c92c1ac14b53b19486586a8070703
oorkan <oorkan.dev@gmail.com>
2021-03-09 18:58:15 +0400

    add 25-EX-blanks_tabs_newlines_count

---
+++ 25-EX-blanks_tabs_newlines_count.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+  int c;
+  double numspaces, numtabs, numlines;
+  numspaces = 0;
+  numtabs = 0;
+  numlines = 0;
+
+  while ( (c = getchar()) != EOF ) {
+  	if ( c == '\n' ) ++numlines;
+  	if ( c == ' ' ) ++numspaces;
+  	if ( c == '	' )	++numtabs;
+  }
+  printf("Spaces: %.0f\n", numspaces);
+  printf("Tabs: %.0f\n", numtabs);
+  printf("Lines: %.0f\n", numlines);
+}
