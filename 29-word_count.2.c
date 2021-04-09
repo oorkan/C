43aaafc7409e4df8d56c2012ef60a64ce325687b
oorkan <oorkan.dev@gmail.com>
2021-04-09 21:25:17 +0400

    #fix is_word -> is_inside_word

--- 29-word_count.2.c
+++ 29-word_count.2.c
@@ -2,15 +2,15 @@
 
 void main()
 {
-  int c, numwords, is_word;
+  int c, numwords, is_inside_word;
 
   while ( (c = getchar()) != EOF ) {
     
     if ( c == ' ' || c == '\n' || c == '\t' ) {
-      is_word = 0;
+      is_inside_word = 0;
     }
-    else if ( is_word == 0 ) {
-      is_word = 1;
+    else if ( is_inside_word == 0 ) {
+      is_inside_word = 1;
       numwords++;
     }
   }
