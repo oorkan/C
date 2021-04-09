6b77b6526bb649998cca8cbba67c6a4cd8cae28d
oorkan <oorkan.dev@gmail.com>
2021-04-09 18:33:47 +0400

    add 26-EX-remove_blanks - #TMP

---
+++ 26-EX-remove_blanks.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main()
+{
+  int c; // Current input char
+  int lc; // Last input char
+
+  while ( (c = getchar()) != EOF ) {
+    // Exit on Enter
+    if ( c == '\n' ) { printf("\n"); break; }
+
+    /*
+      If last input character was blank and current input character is blank,
+      then reassign the current input character
+    */
+    while (c == ' ' &&  lc == ' ' ) {
+      c = getchar(); lc = c;
+    }
+
+    lc = c;
+    putchar(c);
+  }
+}
