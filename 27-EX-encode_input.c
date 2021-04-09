8850abf90fab43a34958351237a36732b58f3843
oorkan <oorkan.dev@gmail.com>
2021-04-09 19:00:41 +0400

    add 27-EX-encode_input - #TMP

---
+++ 27-EX-encode_input.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main()
+{
+  int c;
+
+  while ( (c = getchar()) != EOF ) {
+    // Exit on Enter
+    if ( c == '\n' ) { printf("\n"); break; }
+
+    if ( c == '\t' ) {
+      c = '\\'; putchar(c); c = 't';
+    }
+
+    if ( c == '\b' ) {
+      c = '\\'; putchar(c); c = 'b';
+    }
+
+    if ( c == '\\' ) {
+      c = '\\'; putchar(c); c = '\\';
+    }
+
+    putchar(c);
+  }
+}
