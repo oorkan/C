9725d0910c77d952a5e054d75d306d56652087a8
oorkan <oorkan.dev@gmail.com>
2021-04-09 22:03:34 +0400

    add 31-EX-word_count-one_word_per_line

---
+++ 31-EX-word_count-one_word_per_line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+{
+  int c, numwords, is_inside_word;
+
+  while ( (c = getchar()) != EOF ) {
+    // Exit on Enter
+    if ( c == '\n' ) { printf("\n"); break; }
+    
+    if ( c == ' ' || c == '\n' || c == '\t' ) {
+      is_inside_word = 0;
+    }
+    else 
+      if ( is_inside_word == 0 ) {
+        is_inside_word = 1;
+        putchar('\n');
+      }
+      putchar(c);
+  }
+}
