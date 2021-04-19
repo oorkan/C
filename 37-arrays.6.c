16e7f66ebc1bcf4f0785a4b5c96d52f3f8ef336f
oorkan <oorkan.dev@gmail.com>
2021-04-19 23:03:53 +0400

    add 37-arrays.6

---
+++ 37-arrays.6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+{
+  int c, i, nwhite, nother;
+  int ndigit[10];
+  int len = ( sizeof(ndigit) / sizeof(int) );
+
+  nwhite = nother = 0;
+  for (i = 0; i < len; ++i) ndigit[i] = 0;
+
+  while ( (c = getchar()) != EOF )
+    if (c >= '0' && c <= '9') ++ndigit[c-'0'];
+    else if (c == ' ' || c == '\n' || c == '\t') ++nwhite;
+    else ++nother;
+
+  printf("digits =");
+
+  for (i = 0; i < len; ++i) printf(" %d", ndigit[i]);
+
+  printf(", white space = %d, other = %d\n", nwhite, nother);
+}
